935a2df6561adde87e0f22d1ed3f5721d9583b01
Ria <riaratnasari@outlook.com>
2019-06-13 17:05:58 +0200

    header footer

---
+++ src/Components/Header_footer/Header.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom';
+import {CityLogo} from '../ui/icons';
+class Header extends Component {
+  render() {
+    return (
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: '#98c5e9',
+          boxShadow: 'none',
+          padding: '10px 0',
+          borderBottom: '2px solid #00285e',
+        }}>
+        <ToolBar style={{display: 'flex'}}>
+          <div style={{flexGrow: 1}}>
+            <div className="header_logo">
+              <CityLogo link="true" linkTo="/" width="70px" height="70px" />
+            </div>
+          </div>
+          <Link to="/the_team">
+            <Button color="inherit"> Team</Button>
+          </Link>
+          <Link to="/the_match">
+            <Button color="inherit">Match</Button>
+          </Link>
+        </ToolBar>
+      </AppBar>
+    );
+  }
+}
+
+export default Header;
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Layout from './Hoc/Layout';
+
+const Routes = props => {
+  return <Layout>second line</Layout>;
+};
+
+export default Routes;
